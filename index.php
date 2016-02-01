3df4e4bd70cb7ee0444e27434c99fa16b1c4bf8a
zeromile <jasonleecooksey@gmail.com>
2016-02-01 19:51:37 +0000

    update index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is the title</h1>
+    <h2>This is the subhead</h2>
+</body>
+</html>
